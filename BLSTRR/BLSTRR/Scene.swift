8f9f71dabbfdcb7c9e6a34f9ad5b2953ea7dec9d
jesseleeruiz <jesseleeruiz@gmail.com>
2020-10-30 14:33:09 -1000

    Created targets to show up at random in Scene.swift. Added those in the ViewController in order for them to populate on the screen.

--- BLSTRR/BLSTRR/Scene.swift
+++ BLSTRR/BLSTRR/Scene.swift
@@ -10,8 +10,28 @@ import ARKit
 
 class Scene: SKScene {
     
+    // MARK: - Properties
+    let remainingLabel = SKLabelNode()
+    var timer: Timer?
+    
+    var targetsCreated = 0
+    var targetCount = 0 {
+        didSet {
+            remainingLabel.text = "Remaining: \(targetCount)"
+        }
+    }
+    
     override func didMove(to view: SKView) {
-        // Setup your scene here
+        remainingLabel.fontSize = 36
+        remainingLabel.fontName = "AmericanTypewriter"
+        remainingLabel.color = .white
+        remainingLabel.position = CGPoint(x: 0, y: view.frame.midY - 50)
+        addChild(remainingLabel)
+        targetCount = 0
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true) { timer in
+            self.createTarget()
+        }
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -19,21 +39,40 @@ class Scene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let sceneView = self.view as? ARSKView else {
+        
+    }
+    
+    /// Manages how many targets have been created and ends the game if we've created 20
+    func createTarget() {
+        if targetsCreated == 20 {
+            timer?.invalidate()
+            timer = nil
             return
         }
+        targetsCreated += 1
+        targetCount += 1
         
-        // Create anchor using the camera's current position
-        if let currentFrame = sceneView.session.currentFrame {
-            
-            // Create a transform with a translation of 0.2 meters in front of the camera
-            var translation = matrix_identity_float4x4
-            translation.columns.3.z = -0.2
-            let transform = simd_mul(currentFrame.camera.transform, translation)
-            
-            // Add a new anchor to the session
-            let anchor = ARAnchor(transform: transform)
-            sceneView.session.add(anchor: anchor)
-        }
+        // Find the scene view we are drawing into
+        guard let sceneView = self.view as? ARSKView else { return }
+        
+        // Create a random X rotation
+        let xRotation = simd_float4x4(SCNMatrix4MakeRotation(Float.pi * 2 * Float.random(in: 0 ... 1), 1, 0, 0))
+        
+        // Create a random Y rotation
+        let yRotation = simd_float4x4(SCNMatrix4MakeRotation(Float.pi * 2 * Float.random(in: 0 ... 1), 0, 1, 0))
+        
+        // Combine them together
+        let rotation = simd_mul(xRotation, yRotation)
+        
+        // Move forward 1.5 meters into the screen
+        var translation = matrix_identity_float4x4
+        translation.columns.3.z = -1.5
+        
+        // Combine that with our rotation
+        let transform = simd_mul(rotation, translation)
+        
+        // Create an anchor at the finished position
+        let anchor = ARAnchor(transform: transform)
+        sceneView.session.add(anchor: anchor)
     }
 }
